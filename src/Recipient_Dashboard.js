f307fe1f9d724b9a325b1e4467ad64787b2a1e2e
zairen <zairenmae.ninofranco@cit.edu>
2023-11-19 16:39:34 +0800

    bag o update Donor_Details

---
+++ src/Recipient_Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecipientDashboard = () => {
+  return (
+    <div>
+      {/* Your Recipient Dashboard content goes here */}
+      <h1>Recipient Dashboard</h1>
+    </div>
+  );
+};
+
+export default RecipientDashboard;
