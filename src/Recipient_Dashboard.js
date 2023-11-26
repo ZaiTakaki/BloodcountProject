aace84df77ea6c54068739054a619ab42c35e22d
ZairenMae <zairenmae.ninofranco@cit.edu>
2023-11-26 21:17:56 +0800

    done Recipient_Dashboard

--- src/Recipient_Dashboard.js
+++ src/Recipient_Dashboard.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
+import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import heroLogo from './Image Hero.svg';
 import circleWithBlood from './circlewithblood.png';
+import Vector from './Vector.png';
+import image5 from './image 5.png'; 
+import rectangle53 from './Rectangle 53.png';
+import rectangle11 from './Rectangle 11.png';
+import image10 from './image 10.png'; 
+import rectangle12 from './Rectangle 12.png';
+import rectangle54 from './Rectangle 54.png';
 
 const RecipientDashboard = () => {
   const navigate = useNavigate();
@@ -19,21 +26,14 @@ const RecipientDashboard = () => {
               <span style={{ color: '#861530', fontSize: 20, fontFamily: 'Poppins', fontWeight: '700', letterSpacing: 1, wordWrap: 'break-word' }}>Count</span>
             </div>
           </div>
-
-          {/* TextField with increased marginRight */}
-          <TextField
-            id="outlined-basic"
-            label="Search"
-            variant="outlined"
-            style={{ width: 288, height: 55, background: 'white', borderRadius: 100, border: '1px #FFC3C3 solid', marginLeft: 20, marginRight: 20 }}
-          />
         </div>
 
         {/* Navigation with increased marginRight and aligned to circle-with-blood */}
         <div className="Navigation" style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)', top: '20%', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
-          <div className="Home">Home</div>
-          <div className="Service">Service</div>
-          <div className="About">About</div>
+          <div className="Home" style={{ color: '#FF0000', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word' }}>home</div>
+          <div className="Service">
+            <div className="AvailableDonors" style={{ color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word' }}>Available Donors</div>
+          </div>
         </div>
 
         {/* Buttons with increased marginRight */}
@@ -80,13 +80,75 @@ const RecipientDashboard = () => {
 
         {/* Circle with Label */}
         <Box />
+
+        {/* New div with search for hospitals */}
+        <div className="Frame1460" style={{ width: 280, height: 50, paddingLeft: 10, paddingRight: 10, background: 'white', borderRadius: 100, border: '1px #FFC3C3 solid', justifyContent: 'flex-start', alignItems: 'center', gap: 146, display: 'inline-flex', marginTop: '20px', marginLeft: '-2000px' }}>
+       <div className="SearchForHospitals" style={{ color: 'black', fontSize: 12, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word' }}>Search for Hospitals</div>
+      <img src={Vector} alt="Vector" style={{ width: 17, height: 18 }} />
       </div>
+        </div>
+
+        <div className="Hospitals" style={{width: 226, height: 48, color: '#861530', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word', marginTop: '30px'}}>Hospitals</div>
+        <div className="BloodBank" style={{width: 226, height: 48, color: '#861530', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word', marginLeft: '900px', marginTop: '-45px'}}>Blood Bank</div>
 
       {/* Rest of the content */}
-      <div className="Content">
-        {/* Add your content specific to the Recipient Dashboard page here */}
+      <div className="Content" style={{ marginTop: '20px',marginLeft: '-100px' }}>
+      <img className="image5" style={{ width: 550, height: 140, boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 5, position: 'relative', zIndex: 2 }} src={image5} alt="Blood donation event" />
+      <img className="rectangle53" style={{ width: 'auto', height: 140, boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 5, position: 'relative', zIndex: 2, marginLeft: '100px' }} src={rectangle53} alt="Rectangle 53" />
+      <div className="SubnationalBloodCenterForVisayas" style={{ width: 290, height: 45, textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '39%', left: '69%', transform: 'translate(-50%, -50%)', zIndex: 4 }}>
+              Subnational Blood Center for Visayas
+            </div>
+          <div className="CebuDoctorsUniversityHospital" style={{ textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '50%', left: '25%', transform: 'translate(-50%, -50%)', zIndex: 3 }}>
+            Cebu Doctors’ University Hospital
+          </div>
+          <img className="Rectangle11" style={{width: 550, height: 140, boxShadow: '0px 4px 4px rgba(51, 24.86, 24.86, 0.50)', borderRadius: 5, position: 'relative', zIndex: 4, marginRight: '620px', marginTop: '30px'}} src={rectangle11} alt="Rectangle 11" />
+        </div>
+        <div className="ChongHuaHospitalMandaue" style={{ textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '73%', left: '25%', transform: 'translate(-50%, -50%)', zIndex: 5 }}>
+            Chong Hua Hospital Mandaue
+          </div>
+          <img
+  className="Image10"
+  style={{
+    width: 'auto',
+    height: 140,
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: 5,
+    position: 'absolute',
+    zIndex: 6,
+    left: '68%',  
+    transform: 'translateX(-50%)',  
+    marginTop: '-145px'
+  }}
+  src={image10}
+  alt="Image 10"
+/>
+        <div className="PhilippineRedCrossCebuChapter" style={{ width: 290, height: 45, textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '60%', left: '69%', transform: 'translate(-50%, -50%)', zIndex: 7 }}>
+        Philippine Red Cross, Cebu Chapter
       </div>
-    </div>
+      <img className="Rectangle12" style={{width: 550, height: 140, boxShadow: '0px 4px 4px rgba(51, 24.86, 24.86, 0.50)', borderRadius: 5, position: 'relative', zIndex: 8, marginRight: '715px', marginTop: '30px'}} src={rectangle12} alt="Rectangle 12" />
+      <div className="VicenteSottoMemorialMedicalCenterVsmmc" style={{ textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '96%', left: '25%', transform: 'translate(-50%, -50%)', zIndex: 9 }}>
+      Vicente Sotto Memorial Medical Center(VSMMC)
+          </div>
+          <img
+  className="Rectangle54"
+  style={{
+    width: 'auto',
+    height: 140,
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: 5,
+    position: 'absolute',
+    zIndex: 10,
+    left: '68%',  
+    transform: 'translateX(-50%)',  
+    marginTop: '30px'
+  }}
+  src={rectangle54}
+  alt="Rectangle 54"
+/>
+<div className="VicenteSottoMemorialMedicalCenterVsmmcBloodServicesUnit" style={{ width: 290, height: 45, textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16.10, wordWrap: 'break-word', position: 'absolute', top: '85%', left: '69%', transform: 'translate(-50%, -50%)', zIndex: 11 }}>
+Center(VSMMC) Blood Services Unit
+      </div>
+      </div>
   );
 };
 
